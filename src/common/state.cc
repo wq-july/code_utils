3fc27b1e1fc3a28826e4be178dddc3ab64e6ac18
wq <qiang.wang@zelostech.com>
2024-05-09 17:16:33 +0800

    [feature]: add imu preintegration and adjust some files

--- src/common/state.cc
+++ src/common/state.cc
@@ -4,20 +4,24 @@ namespace Common {
 
 SimpleState::SimpleState() = default;
 
-SimpleState::SimpleState(double time, const Sophus::SO3d& rot,
-                         const Eigen::Vector3d& trans,
+SimpleState::SimpleState(double time, const SO3& rot, const Eigen::Vector3d& trans,
                          const Eigen::Vector3d& vel, const Eigen::Vector3d& bg,
                          const Eigen::Vector3d& ba)
     : timestamp_(time), rot_(rot), trans_(trans), vel_(vel), bg_(bg), ba_(ba) {}
 
-SimpleState::SimpleState(double time, const Sophus::SE3d& pose,
-                         const Eigen::Vector3d& vel)
-    : timestamp_(time),
-      rot_(pose.so3()),
-      trans_(pose.translation()),
-      vel_(vel) {}
+SimpleState::SimpleState(double time, const Eigen::Isometry3d& pose, const Eigen::Vector3d& vel)
+    : timestamp_(time), rot_(pose.rotation()), trans_(pose.translation()), vel_(vel) {}
 
-Sophus::SE3d SimpleState::GetSE3() const { return Sophus::SE3d(rot_, trans_); }
+SimpleState::SimpleState(double time, const Eigen::Matrix3d& rot, const Eigen::Vector3d& pos,
+                         const Eigen::Vector3d& vel)
+    : timestamp_(time), rot_(rot), trans_(pos), vel_(vel) {}
+
+Eigen::Isometry3d SimpleState::GetSE3() const {
+  Eigen::Isometry3d SE3 = Eigen::Isometry3d::Identity();
+  SE3.rotate(rot_.GetMatrix());
+  SE3.pretranslate(trans_);
+  return SE3;
+}
 
 std::ostream& operator<<(std::ostream& os, const SimpleState& s) {
   os << "p: " << s.trans_.transpose() << "\n"
--- src/util/time.cc
+++ src/util/time.cc
@@ -8,62 +8,61 @@ namespace Utils {
 Timer::Timer() : elapsed_time_(0) {}
 
 void Timer::StartTimer(const std::string& description) {
-    timer_description_ = description;
-    if (!timer_description_.empty()) {
-        std::cout << timer_description_ << " Start! " << std::endl;
-    }
-    start_time_ = std::chrono::high_resolution_clock::now();
+  timer_description_ = description;
+  if (!timer_description_.empty()) {
+    std::cout << timer_description_ << " Start! " << std::endl;
+  }
+  start_time_ = std::chrono::high_resolution_clock::now();
 }
 
 void Timer::StopTimer() {
-    end_time_ = std::chrono::high_resolution_clock::now();
-    CalculateElapsedTime();
+  end_time_ = std::chrono::high_resolution_clock::now();
+  CalculateElapsedTime();
 }
 
 double Timer::GetElapsedTime(TimeUnit unit) {
-    switch (unit) {
-        case Microseconds:
-            return std::chrono::duration<double, std::micro>(end_time_ - start_time_).count();
-        case Seconds:
-            return std::chrono::duration<double>(end_time_ - start_time_).count();
-        case Milliseconds:
-        default:
-            return std::chrono::duration<double, std::milli>(end_time_ - start_time_).count();
-    }
+  switch (unit) {
+    case Microseconds:
+      return std::chrono::duration<double, std::micro>(end_time_ - start_time_).count();
+    case Seconds:
+      return std::chrono::duration<double>(end_time_ - start_time_).count();
+    case Milliseconds:
+    default:
+      return std::chrono::duration<double, std::milli>(end_time_ - start_time_).count();
+  }
 }
 
 void Timer::PrintElapsedTime(TimeUnit unit) {
-    std::cout << timer_description_ << " took " << GetElapsedTime(unit);
-    switch (unit) {
-        case Microseconds:
-            std::cout << " us.";
-            break;
-        case Seconds:
-            std::cout << " s.";
-            break;
-        case Milliseconds:
-        default:
-            std::cout << " ms.";
-            break;
-    }
-    std::cout << std::endl;
+  std::cout << timer_description_ << " took " << GetElapsedTime(unit);
+  switch (unit) {
+    case Microseconds:
+      std::cout << " us.";
+      break;
+    case Seconds:
+      std::cout << " s.";
+      break;
+    case Milliseconds:
+    default:
+      std::cout << " ms.";
+      break;
+  }
+  std::cout << std::endl;
 }
 
 void Timer::CalculateElapsedTime() {
-    elapsed_time_ = std::chrono::duration<double, std::milli>(end_time_ - start_time_).count();
+  elapsed_time_ = std::chrono::duration<double, std::milli>(end_time_ - start_time_).count();
 }
 
 std::string Timer::TimestampToReadable(long long timestamp) {
-    std::time_t t = timestamp;
-    std::tm *tm = std::localtime(&t);
-    std::stringstream ss;
-    ss << std::put_time(tm, "%Y-%m-%d %H:%M:%S");
-    return ss.str();
+  std::time_t t = timestamp;
+  std::tm* tm = std::localtime(&t);
+  std::stringstream ss;
+  ss << std::put_time(tm, "%Y-%m-%d %H:%M:%S");
+  return ss.str();
 }
 
 double Timer::TimestampToDoubleSeconds(const uint64_t timestamp) {
   return static_cast<double>(timestamp) / 1.0e9;
 }
 
-
-}
+}  // namespace Utils
