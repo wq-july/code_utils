273b43988444e36040d14e500d2745815709739e
wq <wqjuly@qq.com>
2024-05-26 23:47:20 +0800

    [feature]: add ceres 3d slam and template

--- include/util/utils.h
+++ include/util/utils.h
@@ -13,11 +13,11 @@
 #include <stdexcept>
 #include <vector>
 
-#include "ceres/ceres.h"
 #include "Eigen/Core"
 #include "pcl/point_cloud.h"
 #include "pcl/point_types.h"
 
+#include "ceres/ceres.h"
 #include "glog/logging.h"
 
 namespace Utils {
@@ -130,36 +130,9 @@ void ReadConstraint(std::ifstream* infile, std::vector<Constraint, Allocator>* c
   constraints->push_back(constraint);
 }
 
-// Reads a file in the g2o filename format that describes a pose graph
-// problem. The g2o format consists of two entries, vertices and constraints.
-//
-// In 2D, a vertex is defined as follows:
-//
-// VERTEX_SE2 ID x_meters y_meters yaw_radians
-//
-// A constraint is defined as follows:
-//
-// EDGE_SE2 ID_A ID_B A_x_B A_y_B A_yaw_B I_11 I_12 I_13 I_22 I_23 I_33
-//
-// where I_ij is the (i, j)-th entry of the information matrix for the
-// measurement.
-//
-//
-// In 3D, a vertex is defined as follows:
-//
-// VERTEX_SE3:QUAT ID x y z q_x q_y q_z q_w
-//
-// where the quaternion is in Hamilton form.
-// A constraint is defined as follows:
-//
-// EDGE_SE3:QUAT ID_a ID_b x_ab y_ab z_ab q_x_ab q_y_ab q_z_ab q_w_ab I_11 I_12 I_13 ... I_16 I_22 I_23 ... I_26 ... I_66 // NOLINT
-//
-// where I_ij is the (i, j)-th entry of the information matrix for the
-// measurement. Only the upper-triangular part is stored. The measurement order
-// is the delta position followed by the delta orientation.
 template <typename Pose, typename Constraint, typename MapAllocator, typename VectorAllocator>
 bool ReadG2oFile(const std::string& filename,
-                 std::map<int, Pose, std::less<int>, MapAllocator>* poses,
+                 std::map<int32_t, Pose, std::less<int32_t>, MapAllocator>* poses,
                  std::vector<Constraint, VectorAllocator>* constraints) {
   CHECK(poses != nullptr);
   CHECK(constraints != nullptr);
@@ -193,6 +166,5 @@ bool ReadG2oFile(const std::string& filename,
 
   return true;
 }
-
 
 }  // namespace Utils
